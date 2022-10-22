0f49724ee1d3ee002c6fdc57a51b482686be377f
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-21 22:26:49 -0400

    add better attachment size limits

--- src/content/MessageItem.svelte
+++ src/content/MessageItem.svelte
@@ -75,65 +75,66 @@
               } -mx-0.5 align-middle" />`;
             })}
         </div>
+        {#each message.attachments || [] as attachment}
+          <div
+            class="rounded mt-1 block"
+            style="max-width:90vw;max-height:50vh;{['width', 'height']
+              .map(
+                (h) =>
+                  h +
+                  ':' +
+                  Math.floor(
+                    Math.min(
+                      1,
+                      Math.min(
+                        //@ts-ignore
+                        (window.innerWidth * 0.9) / attachment.metadata.width,
+                        //@ts-ignore
+                        (window.innerHeight * 0.7) / attachment.metadata.height
+                      )
+                    ) *
+                      //@ts-ignore
+                      attachment.metadata[h]
+                  ) +
+                  'px'
+              )
+              .join(';')}"
+          >
+            {#if attachment.metadata.type == "Image"}
+              <img
+                class="block rounded"
+                style="max-width:inherit;max-height:inherit;width:inherit;height:inherit;"
+                src={proxyURL(
+                  client.generateFileURL(attachment, {
+                    width: Math.floor(window.innerWidth * 0.9),
+                  }),
+                  "image"
+                )}
+                alt={attachment.filename}
+              />
+            {:else if attachment.metadata.type == "Video"}
+              <!-- svelte-ignore a11y-media-has-caption -->
+              <video
+                class="block"
+                src={proxyURL(client.generateFileURL(attachment), "any")}
+                alt={attachment.filename}
+                controls
+              />
+            {:else if attachment.metadata.type == "Audio"}
+              <audio
+                class="block"
+                src={proxyURL(client.generateFileURL(attachment), "any")}
+                alt={attachment.filename}
+                controls
+              />
+            {:else}
+              <a href={client.generateFileURL(attachment)} target="_blank"
+                >Download {attachment.filename}</a
+              >
+            {/if}
+          </div>
+        {/each}
       </div>
     </div>
-    {#each message.attachments || [] as attachment}
-      <div
-        class="rounded mt-2 block"
-        style="max-width:90vw;{['width', 'height']
-          .map(
-            (h) =>
-              h +
-              ':' +
-              Math.floor(
-                Math.min(
-                  1,
-                  Math.min(
-                    //@ts-ignore
-                    (window.innerWidth * 0.9) / attachment.metadata.width,
-                    //@ts-ignore
-                    (window.innerHeight * 0.7) / attachment.metadata.height
-                  )
-                ) *
-                  //@ts-ignore
-                  attachment.metadata[h]
-              ) +
-              'px'
-          )
-          .join(';')}"
-      >
-        {#if attachment.metadata.type == "Image"}
-          <img
-            class="block rounded"
-            src={proxyURL(
-              client.generateFileURL(attachment, {
-                width: Math.floor(window.innerWidth * 0.9),
-              }),
-              "image"
-            )}
-            alt={attachment.filename}
-          />
-        {:else if attachment.metadata.type == "Video"}
-          <!-- svelte-ignore a11y-media-has-caption -->
-          <video
-            class="block"
-            src={proxyURL(client.generateFileURL(attachment), "any")}
-            alt={attachment.filename}
-            controls
-          />
-        {:else if attachment.metadata.type == "Audio"}
-          <audio
-            class="block"
-            src={proxyURL(client.generateFileURL(attachment), "any")}
-            alt={attachment.filename}
-            controls
-          />
-        {:else}
-          <a href={client.generateFileURL(attachment)} target="_blank"
-            >Download {attachment.filename}</a
-          >
-        {/if}
-      </div>
-    {/each}
   </div>
 {/if}
