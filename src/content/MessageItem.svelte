d93bd7bdcc01c05ff0e83534f77294bf037cbc88
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-28 18:15:43 -0400

    fix mobile buttons too small

--- src/content/MessageItem.svelte
+++ src/content/MessageItem.svelte
@@ -137,12 +137,16 @@
     </div>
     {#if $HoveredMessage == message._id}
       <div
-        class="absolute -top-4 right-3 h-8 flex rounded-lg"
+        class="absolute {$MobileLayout
+          ? '-top-6 right-5 h-12'
+          : '-top-4 right-3 h-8'} flex rounded-lg"
         data-hover-item
         style="background-color:{$Theme['primary-header']};"
       >
         <div
-          class="rounded-lg h-8 w-8 bg-inherit cursor-pointer flex items-center justify-center hover:brightness-90 active:brightness-75"
+          class="rounded-lg {$MobileLayout
+            ? 'h-12 w-12'
+            : 'h-8 w-8'} bg-inherit cursor-pointer flex items-center justify-center hover:brightness-90 active:brightness-75"
           on:click={() => {
             updateReplies(message);
             selectBottom();
