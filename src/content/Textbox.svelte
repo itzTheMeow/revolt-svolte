0d0ca70259ea9a02f4c824f8070a7eef8f4bd581
Meow VPS <undefined@null.com>
2022-11-01 23:27:46 -0400

    fix maybe

--- src/content/Textbox.svelte
+++ src/content/Textbox.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
   import { client } from "Client";
   import { uploadAttachment } from "revolt-toolset";
-  import { parseAutocomplete, type AutocompleteTabResult } from "revolt-toolset/dist/autocomplete";
+  import {
+    parseAutocomplete,
+    type AutocompleteTabResult,
+  } from "revolt-toolset/dist/autocomplete";
   import {
     autocomplete,
     MessageInputSelected,
     pendBottom,
     pushFile,
     replyingTo,
+    selectBottom,
     SelectedChannel,
     SelectedServer,
     selectInput,
     uploadedFiles,
   } from "State";
-  import { ArrowBigRightLine, Hash, Paperclip, Volume } from "tabler-icons-svelte";
+  import {
+    ArrowBigRightLine,
+    Hash,
+    Paperclip,
+    Volume,
+  } from "tabler-icons-svelte";
   import { Theme } from "Theme";
   import { proxyURL } from "utils";
   import AutocompleteItem from "./AutocompleteItem.svelte";
@@ -27,7 +36,11 @@
   function recalculateAutocomplete() {
     if (!$MessageInputSelected) return autocomplete.set(null);
     autocomplete.set(
-      parseAutocomplete($SelectedServer!, inputtedMessage, MessageInput.selectionStart || 0)
+      parseAutocomplete(
+        $SelectedServer!,
+        inputtedMessage,
+        MessageInput.selectionStart || 0
+      )
     );
   }
   function handleAutocomplete(e: KeyboardEvent) {
@@ -35,7 +48,11 @@
       e.preventDefault();
       handleAutocompleteTab(
         $autocomplete.tab(
-          [...$autocomplete.channels, ...$autocomplete.emojis, ...$autocomplete.users][0]
+          [
+            ...$autocomplete.channels,
+            ...$autocomplete.emojis,
+            ...$autocomplete.users,
+          ][0]
         )
       );
       recalculateAutocomplete();
@@ -89,6 +106,7 @@
     replyingTo.set([]);
     recalculateAutocomplete();
   }
+
 </script>
 
 <TextboxUploaded />
@@ -98,38 +116,37 @@
 {#if $autocomplete?.size}
   <div
     class="overflow-y-auto py-2 w-full"
-    style="max-height:35%;background-color:{$Theme['primary-header']};"
-  >
+    style="max-height:35%;background-color:{$Theme['primary-header']};">
     {#each $autocomplete.channels.slice(0, 15) as c}
       <AutocompleteItem
         icon={c.icon
-          ? proxyURL(c.generateIconURL({ max_side: 64 }), "image")
-          : c.channel_type == "VoiceChannel"
+          ? proxyURL(c.generateIconURL({ max_side: 64 }), 'image')
+          : c.channel_type == 'VoiceChannel'
           ? Volume
           : Hash}
-        name={c.name || ""}
-        onclick={() => handleAutocompleteTab($autocomplete?.tab(c))}
-      />
+        name={c.name || ''}
+        onclick={() => handleAutocompleteTab($autocomplete?.tab(c))} />
     {/each}
     {#each $autocomplete.emojis.slice(0, 15) as e}
       <AutocompleteItem
-        icon={proxyURL(e.imageURL, "image")}
-        name={e.name || ""}
-        detail={e.parent.type == "Server" ? client.servers.get(e.parent.id)?.name || "" : ""}
-        onclick={() => handleAutocompleteTab($autocomplete?.tab(e))}
-      />
+        icon={proxyURL(e.imageURL, 'image')}
+        name={e.name || ''}
+        detail={e.parent.type == 'Server'
+          ? client.servers.get(e.parent.id)?.name || ''
+          : ''}
+        onclick={() => handleAutocompleteTab($autocomplete?.tab(e))} />
     {/each}
     {#each $autocomplete.users.slice(0, 15) as u}
       <AutocompleteItem
         icon={proxyURL(
-          u.generateAvatarURL({ max_side: 64 }) || u.user?.generateAvatarURL({ max_side: 64 }),
-          "image"
+          u.generateAvatarURL({ max_side: 64 }) ||
+            u.user?.generateAvatarURL({ max_side: 64 }),
+          'image'
         )}
-        name={u.nickname || u.user?.username || ""}
-        detail={u.user?.username || ""}
+        name={u.nickname || u.user?.username || ''}
+        detail={u.user?.username || ''}
         rounded
-        onclick={() => handleAutocompleteTab($autocomplete?.tab(u))}
-      />
+        onclick={() => handleAutocompleteTab($autocomplete?.tab(u))} />
     {/each}
   </div>
 {/if}
@@ -155,13 +172,11 @@
     on:change={() => {
       const files = [...(FileInput.files || [])];
       files.forEach(pushFile);
-    }}
-  />
+    }} />
   <div
     class="btn btn-square btn-secondary rounded-none border-none"
     style="background-color:{$Theme['primary-header']};"
-    on:click={() => FileInput.click()}
-  >
+    on:click={() => FileInput.click()}>
     <Paperclip />
   </div>
   <input
@@ -175,7 +190,7 @@
     on:keydown={recalculateAutocomplete}
     on:keyup={(e) => {
       if (handleAutocomplete(e)) return;
-      if (e.key == "Enter") sendMessage();
+      if (e.key == 'Enter') sendMessage();
       recalculateAutocomplete();
     }}
     on:touchmove={() => recalculateAutocomplete()}
@@ -183,21 +198,18 @@
     on:mouseup={() => recalculateAutocomplete()}
     on:click={() => MessageInputSelected.set(true)}
     on:focus={() => MessageInputSelected.set(true)}
-    on:focusin={() => MessageInputSelected.set(true)}
-    on:blur={() => MessageInputSelected.set(false)}
-  />
+    on:blur={() => MessageInputSelected.set(false)} />
   <div
     class="btn btn-square btn-primary rounded-none border-none"
     style="background-color:{$Theme['accent']};"
     bind:this={SendButton}
     on:touchstart={() => {
-      if (!$MessageInputSelected) MessageInput.focus();
+      if (!$MessageInputSelected) selectBottom();
     }}
     on:touchend={() => {
-      if (!$MessageInputSelected) MessageInput.focus();
+      if (!$MessageInputSelected) selectBottom();
     }}
-    on:click={() => sendMessage()}
-  >
+    on:click={() => sendMessage()}>
     <ArrowBigRightLine />
   </div>
 </div>
