bf9b9da612abe237d05585fa775048d9c933940c
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-11-01 22:52:44 -0400

    replies

--- src/content/MessageItem.svelte
+++ src/content/MessageItem.svelte
@@ -6,17 +6,21 @@
   import MessageItemAttachments from "./MessageItemAttachments.svelte";
   import MessageItemContent from "./MessageItemContent.svelte";
   import MessageItemHeader from "./MessageItemHeader.svelte";
+  import MessageItemReplies from "./MessageItemReplies.svelte";
   import MessageItemToolbar from "./MessageItemToolbar.svelte";
 
   export let message: Message;
 
+  let isReply = false;
   let shouldSeparate = true;
   $: {
     const previousMessage =
       $MessageCache[$SelectedChannel!._id]?.[
         $MessageCache[$SelectedChannel!._id]?.indexOf(message) - 1
       ];
+    isReply = !!message.reply_ids?.length;
     shouldSeparate =
+      isReply ||
       !previousMessage ||
       previousMessage.author_id !== message.author_id ||
       JSON.stringify(previousMessage.masquerade) !== JSON.stringify(message.masquerade);
@@ -24,42 +28,47 @@
 </script>
 
 {#if $SelectedChannel}
-  <div
-    class="relative {shouldSeparate ? 'mt-3' : ''}"
-    style={$HoveredMessage == message._id
-      ? `background-color:${$Theme["secondary-background"]};`
-      : ""}
-    on:mouseenter={() => HoveredMessage.set(message._id)}
-    on:mousemove={() => HoveredMessage.set(message._id)}
-    on:mouseleave={() => HoveredMessage.set(null)}
-    on:wheel={() => HoveredMessage.set(message._id)}
-    on:click={(e) => {
-      if (!$MobileLayout) return;
-      HoveredMessage.set(message._id);
-      e.preventDefault();
-      return false;
-    }}
-  >
-    <div class="flex gap-2">
-      {#if shouldSeparate}
-        <img
-          class="rounded-full h-10 w-10 shrink-0 object-cover"
-          src={MessageDetails(message).avatar}
-          alt=""
-        />
-      {:else}
-        <div class="h-0.5 w-10 shrink-0" />
-      {/if}
-      <div class="flex flex-col flex-1">
-        {#if shouldSeparate}
-          <MessageItemHeader {message} />
-        {/if}
-        <MessageItemContent {message} />
-        <MessageItemAttachments {message} />
-      </div>
-    </div>
-    {#if $HoveredMessage == message._id}
-      <MessageItemToolbar {message} />
+  <div>
+    {#if isReply}
+      <MessageItemReplies {message} />
     {/if}
+    <div
+      class="relative {shouldSeparate ? 'mt-3' : ''}"
+      style={$HoveredMessage == message._id
+        ? `background-color:${$Theme["secondary-background"]};`
+        : ""}
+      on:mouseenter={() => HoveredMessage.set(message._id)}
+      on:mousemove={() => HoveredMessage.set(message._id)}
+      on:mouseleave={() => HoveredMessage.set(null)}
+      on:wheel={() => HoveredMessage.set(message._id)}
+      on:click={(e) => {
+        if (!$MobileLayout) return;
+        HoveredMessage.set(message._id);
+        e.preventDefault();
+        return false;
+      }}
+    >
+      <div class="flex gap-2">
+        {#if shouldSeparate}
+          <img
+            class="rounded-full h-10 w-10 shrink-0 object-cover"
+            src={MessageDetails(message).avatar}
+            alt=""
+          />
+        {:else}
+          <div class="h-0.5 w-10 shrink-0" />
+        {/if}
+        <div class="flex flex-col flex-1">
+          {#if shouldSeparate}
+            <MessageItemHeader {message} />
+          {/if}
+          <MessageItemContent {message} />
+          <MessageItemAttachments {message} />
+        </div>
+      </div>
+      {#if $HoveredMessage == message._id}
+        <MessageItemToolbar {message} />
+      {/if}
+    </div>
   </div>
 {/if}
---
+++ src/content/MessageItemReplies.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import type { Message } from "revolt.js";
+  import { MessageCache } from "State";
+  import { CornerLeftDown } from "tabler-icons-svelte";
+  import { Theme } from "Theme";
+  import { MessageDetails, proxyURL } from "utils";
+
+  export let message: Message;
+
+  let replies: (Message | undefined)[] =
+    message.reply_ids?.map((i, ii) => {
+      const cached = $MessageCache[message.channel_id].find((m) => m._id == i);
+      if (!cached) {
+        message.channel?.fetchMessage(i).then((m) => {
+          replies[ii] = m;
+        });
+      }
+      return cached;
+    }) || [];
+</script>
+
+<div class="mt-3 -mb-2.5 pl-1.5">
+  {#each replies.sort((r1, r2) => (r1?.createdAt || 0) - (r2?.createdAt || 0)) as reply}
+    <div class="flex gap-2 items-center">
+      <CornerLeftDown size={18} />
+      <img
+        class="w-4 h-4 rounded-full -ml-1"
+        src={reply
+          ? MessageDetails(reply).avatar
+          : proxyURL(
+              `https://api.revolt.chat/users/${
+                message.reply_ids?.[replies.indexOf(reply)]
+              }/default_avatar`,
+              "image"
+            )}
+        alt=""
+      />
+      <div style="color:{reply ? MessageDetails(reply).color || 'inherit' : 'inherit'};">
+        {reply ? MessageDetails(reply).name : "Unknown User"}
+      </div>
+      <div class="flex-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
+        {#if message.content}
+          {message.content.slice(0, 200)}
+        {:else}
+          <i>Sent an Attachment</i>
+        {/if}
+      </div>
+    </div>
+  {/each}
+</div>
