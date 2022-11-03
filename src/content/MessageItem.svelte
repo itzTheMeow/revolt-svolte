c96a9fd5ee74eb37814600c5335cabc5acb8714c
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-11-02 22:01:03 -0400

    un-div messages

--- src/content/MessageItem.svelte
+++ src/content/MessageItem.svelte
@@ -28,47 +28,45 @@
 </script>
 
 {#if $SelectedChannel}
-  <div>
-    {#if isReply}
-      <MessageItemReplies {message} />
-    {/if}
-    <div
-      class="relative {shouldSeparate ? 'mt-3' : ''}"
-      style={$HoveredMessage == message._id
-        ? `background-color:${$Theme["secondary-background"]};`
-        : ""}
-      on:mouseenter={() => HoveredMessage.set(message._id)}
-      on:mousemove={() => HoveredMessage.set(message._id)}
-      on:mouseleave={() => HoveredMessage.set(null)}
-      on:wheel={() => HoveredMessage.set(message._id)}
-      on:click={(e) => {
-        if (!$MobileLayout) return;
-        HoveredMessage.set(message._id);
-        e.preventDefault();
-        return false;
-      }}
-    >
-      <div class="flex gap-2">
-        {#if shouldSeparate}
-          <img
-            class="rounded-full h-10 w-10 shrink-0 object-cover"
-            src={MessageDetails(message).avatar}
-            alt=""
-          />
-        {:else}
-          <div class="h-0.5 w-10 shrink-0" />
-        {/if}
-        <div class="flex flex-col flex-1">
-          {#if shouldSeparate}
-            <MessageItemHeader {message} />
-          {/if}
-          <MessageItemContent {message} />
-          <MessageItemAttachments {message} />
-        </div>
-      </div>
-      {#if $HoveredMessage == message._id}
-        <MessageItemToolbar {message} />
+  {#if isReply}
+    <MessageItemReplies {message} />
+  {/if}
+  <div
+    class="relative {shouldSeparate ? 'mt-3' : ''}"
+    style={$HoveredMessage == message._id
+      ? `background-color:${$Theme["secondary-background"]};`
+      : ""}
+    on:mouseenter={() => HoveredMessage.set(message._id)}
+    on:mousemove={() => HoveredMessage.set(message._id)}
+    on:mouseleave={() => HoveredMessage.set(null)}
+    on:wheel={() => HoveredMessage.set(message._id)}
+    on:click={(e) => {
+      if (!$MobileLayout) return;
+      HoveredMessage.set(message._id);
+      e.preventDefault();
+      return false;
+    }}
+  >
+    <div class="flex gap-2">
+      {#if shouldSeparate}
+        <img
+          class="rounded-full h-10 w-10 shrink-0 object-cover"
+          src={MessageDetails(message).avatar}
+          alt=""
+        />
+      {:else}
+        <div class="h-0.5 w-10 shrink-0" />
       {/if}
+      <div class="flex flex-col flex-1">
+        {#if shouldSeparate}
+          <MessageItemHeader {message} />
+        {/if}
+        <MessageItemContent {message} />
+        <MessageItemAttachments {message} />
+      </div>
     </div>
+    {#if $HoveredMessage == message._id}
+      <MessageItemToolbar {message} />
+    {/if}
   </div>
 {/if}
