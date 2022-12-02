4254c81af3df70565d793655053bf1403eb193c7
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-01 19:29:03 -0500

    fix overflow

--- src/memberbar/MemberBarItem.svelte
+++ src/memberbar/MemberBarItem.svelte
@@ -7,19 +7,22 @@
 </script>
 
 {#if item instanceof Member}
-  <div class="inline-flex gap-1 items-center mb-1.5 select-none">
+  <div class="inline-flex gap-1.5 w-full items-center mb-1.5 select-none">
     <img
       class="rounded-full h-10 w-10 object-cover"
       src={MemberDetails(item).avatar}
       alt={item.user?.username}
     />
-    <div class="flex flex-col">
-      <div class="font-semibold" style:color={MemberDetails(item).color || "inherit"}>
+    <div class="flex flex-col" style:width="calc(100% - 2.5rem - 0.375rem)">
+      <div
+        class="font-semibold overflow-hidden whitespace-nowrap overflow-ellipsis"
+        style:color={MemberDetails(item).color || "inherit"}
+      >
         {MemberDetails(item).name}
       </div>
       {#if item.user?.status?.text && UserDetails(item.user).online}
         <div
-          class="text-xs overflow-hidden whitespace-nowrap overflow-ellipsis"
+          class="text-xs w-full overflow-hidden whitespace-nowrap overflow-ellipsis"
           style:color={$Theme["tertiary-foreground"]}
         >
           {item.user.status.text}
@@ -28,7 +31,10 @@
     </div>
   </div>
 {:else}
-  <div class="inline-block mb-1 font-bold select-none" style:color={item.colour || "inherit"}>
+  <div
+    class="block mb-1 font-bold select-none overflow-hidden whitespace-nowrap overflow-ellipsis"
+    style:color={item.colour || "inherit"}
+  >
     {item.name}{item.count > 1 ? ` - ${item.count.toLocaleString()}` : ""}
   </div>
 {/if}
