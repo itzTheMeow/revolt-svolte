89f6be939a67bb17e420fa3c41e49d5272518bfe
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-18 12:48:52 -0500

    add server banner/title

--- src/channels/ChannelList.svelte
+++ src/channels/ChannelList.svelte
@@ -2,46 +2,58 @@
   import { CollapsedCategories, SelectedServer } from "State";
   import { ChevronDown, ChevronRight } from "tabler-icons-svelte";
   import { Theme } from "Theme";
+  import { proxyURL } from "utils";
   import ChannelItem from "./ChannelItem.svelte";
-
-  $: {
-    console.log($SelectedServer?.orderedChannels);
-  }
 </script>
 
 <div
-  class="overflow-y-auto h-full w-56 py-1 select-none"
+  class="h-full w-56 select-none flex flex-col"
   style="background-color:{$Theme['secondary-background']}"
   id="ChannelList"
 >
   {#if $SelectedServer}
-    {#each $SelectedServer.orderedChannels as category}
-      {#if $SelectedServer.orderedChannels.indexOf(category) && category.title !== "Default"}
-        <div
-          class="text-base text-primary ml-2 mt-0.5 {$CollapsedCategories.includes(category.id)
-            ? 'mb-1'
-            : '-mb-0.5'} font-bold flex cursor-pointer items-center gap-0.5"
-          style="color:{$Theme['accent']};"
-          on:click={() => {
-            if ($CollapsedCategories.includes(category.id))
-              $CollapsedCategories = $CollapsedCategories.filter((c) => c !== category.id);
-            else $CollapsedCategories = [...$CollapsedCategories, category.id];
-          }}
-        >
-          {#if $CollapsedCategories.includes(category.id)}
-            <ChevronRight size={16} />
-          {:else}
-            <ChevronDown size={16} />
-          {/if}
-          {category.title}
-        </div>
-      {/if}
-      {#if !$CollapsedCategories.includes(category.id)}
-        {#each category.channels as channel}
-          <ChannelItem {channel} />
-        {/each}
-      {/if}
-    {/each}
+    <div
+      class="w-full {$SelectedServer.banner ? 'h-28' : 'h-10'} bg-cover bg-center flex"
+      style:background-image={$SelectedServer.banner
+        ? `url(${proxyURL($SelectedServer.generateBannerURL({ max_side: 480 }), "image")})`
+        : ""}
+    >
+      <div
+        class="w-full mt-auto flex items-center pb-0.5 pt-1"
+        style:background="linear-gradient(0deg, {$Theme["secondary-background"]}, transparent)"
+      >
+        <div class="font-bold text-lg w-full ml-2">{$SelectedServer.name}</div>
+      </div>
+    </div>
+    <div class="py-1 overflow-y-auto flex-1">
+      {#each $SelectedServer.orderedChannels as category}
+        {#if $SelectedServer.orderedChannels.indexOf(category) && category.title !== "Default"}
+          <div
+            class="text-base text-primary ml-2 mt-0.5 {$CollapsedCategories.includes(category.id)
+              ? 'mb-1'
+              : '-mb-0.5'} font-bold flex cursor-pointer items-center gap-0.5"
+            style="color:{$Theme['accent']};"
+            on:click={() => {
+              if ($CollapsedCategories.includes(category.id))
+                $CollapsedCategories = $CollapsedCategories.filter((c) => c !== category.id);
+              else $CollapsedCategories = [...$CollapsedCategories, category.id];
+            }}
+          >
+            {#if $CollapsedCategories.includes(category.id)}
+              <ChevronRight size={16} />
+            {:else}
+              <ChevronDown size={16} />
+            {/if}
+            {category.title}
+          </div>
+        {/if}
+        {#if !$CollapsedCategories.includes(category.id)}
+          {#each category.channels as channel}
+            <ChannelItem {channel} />
+          {/each}
+        {/if}
+      {/each}
+    </div>
   {:else}
     <div class="text-sm ml-1.5">No channels.</div>
   {/if}
