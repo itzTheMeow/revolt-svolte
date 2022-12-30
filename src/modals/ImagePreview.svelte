636bec1479e93649f90f980be3f330cdf86bb10a
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-30 15:20:33 -0500

    add image magnifier

--- src/modals/ImagePreview.svelte
+++ src/modals/ImagePreview.svelte
@@ -1,27 +1,79 @@
 <script lang="ts">
+  /* Shamelessly 'inspired' from https://www.w3schools.com/howto/howto_js_image_magnifier_glass.asp */
+
   import { client } from "Client";
+  import { MobileLayout } from "State";
+  import { Theme } from "Theme";
   import { proxyURL } from "utils";
   import { imagePreview } from "./ImagePreview";
 
-  let preview: HTMLDivElement;
+  let preview: HTMLDivElement, image: HTMLImageElement, glass: HTMLDivElement;
 
-  let imageURL: string;
+  let imageURL: string,
+    mouseDown = false,
+    isMagnifying = false,
+    magPos = { x: 0, y: 0 },
+    zoom = 3,
+    glassW = 0,
+    glassH = 0;
   $: {
     if ($imagePreview) {
       imageURL = proxyURL(client.generateFileURL($imagePreview), "image");
+      glassW = glass?.offsetWidth / 2;
+      glassH = glass?.offsetHeight / 2;
     }
+  }
+
+  function calculatePos(e: MouseEvent) {
+    if (!image) return;
+    const rect = image.getBoundingClientRect();
+    let x = e.pageX - rect.left,
+      y = e.pageY - rect.top;
+    if (x > image.width - glassW / zoom) x = image.width - glassW / zoom;
+    if (x < glassW / zoom) x = glassW / zoom;
+    if (y > image.height - glassH / zoom) y = image.height - glassH / zoom;
+    if (y < glassH / zoom) y = glassH / zoom;
+    magPos = { x, y };
   }
 </script>
 
 {#if $imagePreview}
   <div
     class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex items-center justify-center"
-    on:click={(e) =>
+    on:mouseup={(e) => {
       //@ts-ignore
-      !preview.contains(e.target) && imagePreview.set(null)}
+      !preview.contains(e.target) && !mouseDown && imagePreview.set(null);
+      mouseDown = isMagnifying = false;
+    }}
   >
     <div class="max-w-[90%] max-h-[90%]" bind:this={preview}>
-      <img src={imageURL} alt={$imagePreview.filename} />
+      <div class="w-fit h-fit relative">
+        <img
+          bind:this={image}
+          src={imageURL}
+          alt={$imagePreview.filename}
+          class={isMagnifying ? "brightness-75 cursor-none" : "cursor-zoom-in"}
+          on:mousedown={(e) =>
+            e.button == 0 && !$MobileLayout && (mouseDown = isMagnifying = true) && calculatePos(e)}
+          on:mouseenter={(e) => !$MobileLayout && (isMagnifying = mouseDown) && calculatePos(e)}
+          on:mouseleave={() => !$MobileLayout && (isMagnifying = false)}
+          on:mousemove={calculatePos}
+        />
+        {#if isMagnifying}
+          <div
+            class="absolute w-24 h-24 rounded-full pointer-events-none bg-no-repeat"
+            style:border="2px solid {$Theme["accent"]}"
+            style:left={magPos.x - glassW + "px"}
+            style:top={magPos.y - glassH + "px"}
+            style:background-image="url({imageURL})"
+            style:background-position="-{magPos.x * zoom - glassW + 0}px -{magPos.y * zoom -
+              glassH +
+              0}px"
+            style:background-size="{image.width * zoom}px {image.height * zoom}px"
+            bind:this={glass}
+          />
+        {/if}
+      </div>
     </div>
   </div>
 {/if}
