96cc6b5ff3f41062f302761ae7ff5e449972e98a
Meow <me@itsmeow.cat>
2023-01-02 01:46:08 -0500

    fix

--- src/content/MessageItemToolbarItem.svelte
+++ src/content/MessageItemToolbarItem.svelte
@@ -13,7 +13,11 @@
     ? 'h-12 w-12'
     : 'h-10 w-10'} bg-inherit cursor-pointer flex items-center justify-center hover:brightness-90 active:brightness-75"
   {style}
-  on:click={(e) => {
+  on:touchend|preventDefault={(e) => {
+    if (e.shiftKey) dispatch("shiftclick", e);
+    else dispatch("click", e);
+  }}
+  on:click|preventDefault={(e) => {
     if (e.shiftKey) dispatch("shiftclick", e);
     else dispatch("click", e);
   }}
