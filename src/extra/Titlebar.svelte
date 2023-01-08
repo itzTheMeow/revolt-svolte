132e687b498cd2ae2353184d7896ce44a85fac7f
Meow <me@itsmeow.cat>
2023-01-08 06:22:38 -0500

    add logo to titlebar

--- src/extra/Titlebar.svelte
+++ src/extra/Titlebar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { ElectronFullscreen, Native } from "Native";
   import { Maximize, Minimize, Minus, X } from "tabler-icons-svelte";
+  import { Theme } from "Theme";
   import { BRAND_NAME } from "utils";
 </script>
 
@@ -8,7 +9,8 @@
   class="w-full flex items-center pl-2 bg-inherit select-none"
   style="height:{Native.titlebarHeight}px;border-bottom:1px solid rgba(0,0,0,0.2);"
 >
-  <div class="flex-1" style="-webkit-app-region:drag;">
+  <div class="flex-1 flex items-center gap-1" style="-webkit-app-region:drag;">
+    <img class="w-5 h-5" src="/logo.png" />
     <div class="font-semibold">{BRAND_NAME}</div>
   </div>
   <div
@@ -30,8 +32,8 @@
     {/if}
   </div>
   <div
-    class="cursor-pointer hover:bg-error transition-colors active:brightness-75 rounded-none h-full flex items-center justify-center"
-    style="width:{Native.titlebarHeight * 1.5}px;"
+    class="cursor-pointer hover:bg-[var(--err)] transition-colors active:brightness-75 rounded-none h-full flex items-center justify-center"
+    style="width:{Native.titlebarHeight * 1.5}px;--err:{$Theme['error']}"
     on:click={Native.close}
   >
     <X size={Native.titlebarHeight - 8} />
