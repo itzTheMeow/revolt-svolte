93435f45ed9038c46b03a298b13de317efc4cb5b
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-26 13:32:53 -0500

    start popup with buttons

--- src/modals/UserModal.svelte
+++ src/modals/UserModal.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import { IconX } from "@tabler/icons-svelte";
+  import { IconMessageShare, IconUserMinus, IconUserPlus, IconX } from "@tabler/icons-svelte";
   import { client } from "Client";
   import Indicator from "extra/Indicator.svelte";
   import Loader from "Loader.svelte";
-  import type { User, UserProfile } from "revolt-toolset";
+  import { RelationshipStatus, type User, type UserProfile } from "revolt-toolset";
   import { MobileLayout } from "State";
   import { Theme } from "Theme";
   import { proxyURL, StatusColor, UserDetails } from "utils";
   import ModalBase from "./ModalBase.svelte";
   import type { Modal, ModalData } from "./ModalStack";
+  import UserModalAction from "./UserModalAction.svelte";
 
   export let modal: Extract<ModalData, { type: "user" }>;
   let item: Modal,
@@ -39,27 +40,43 @@
     <div class="w-full h-full flex items-center justify-center"><Loader /></div>
   {:then _}
     <div
-      class="flex items-center w-full h-32 bg-cover bg-center p-4 relative"
+      class="flex items-center w-full h-40 bg-cover bg-center px-5 relative"
       style:background-image={profile?.background
         ? `url(${proxyURL(profile.generateBackgroundURL({ max_side: 256 }) || "", "image")})`
         : ""}
       style:background-color={$Theme["secondary-background"]}
     >
       <div
-        class="rounded-full w-16 h-16 relative"
-        style:background-color={$Theme["primary-background"]}
+        class="rounded-xl px-3 py-2 flex items-center gap-1.5"
+        style:background-color={$Theme["background"]}
       >
-        <img
-          class="avatar rounded-full w-full h-full object-cover"
-          src={UserDetails(user).avatar}
-          alt={UserDetails(user).name}
-        />
-        <Indicator
-          pos="bottomRight"
-          color={$Theme[StatusColor(user)]}
-          bg="transparent"
-          className="h-6 w-6 -right-0.5 -bottom-0.5"
-        />
+        <div class="rounded-full w-14 h-14 relative bg-inherit">
+          <img
+            class="avatar rounded-full w-full h-full object-cover"
+            src={UserDetails(user).avatar}
+            alt={UserDetails(user).name}
+          />
+          <Indicator
+            pos="bottomRight"
+            color={$Theme[StatusColor(user)]}
+            bg={$Theme["background"]}
+            className="h-6 w-6 -right-0.5 -bottom-0.5"
+          />
+        </div>
+        <div class="flex flex-col gap-0.5">
+          <div class="font-semibold text-xl">{UserDetails(user).name}</div>
+          {#if user.status}
+            <div>{user.status}</div>
+          {/if}
+        </div>
+      </div>
+      <div class="ml-auto flex gap-2 items-center">
+        {#if user.relationship !== RelationshipStatus.Self}
+          <UserModalAction
+            icon={user.relationship == RelationshipStatus.Friend ? IconUserMinus : IconUserPlus}
+          />
+        {/if}
+        <UserModalAction icon={IconMessageShare} />
       </div>
     </div>
   {:catch err}
---
+++ src/modals/UserModalAction.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Theme } from "Theme";
+
+  export let icon: ConstructorOfATypedSvelteComponent;
+</script>
+
+<div
+  class="rounded-lg p-1.5 flex items-center gap-1.5 hover:brightness-50 transition cursor-pointer"
+  style:background-color={$Theme["background"]}
+>
+  <svelte:component this={icon} />
+</div>
